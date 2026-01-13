a86518d4b34c20d516f9d578499b7b35b8a81609
unknown <bkbagchi.dipto@gmail.com>
2026-01-13 21:05:38 +0600

    new route made for product with loading

---
+++ app/products/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductSkeleton from "../components/skeleton/ProductSkeleton";
+
+const loading = () => {
+  return (
+    <div className='"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      {[...Array(6)].map((_, index) => (
+        <ProductSkeleton key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default loading;
