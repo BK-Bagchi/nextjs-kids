d8c49e909de367bc8b9f7d7684fdd92e6545119f
unknown <bkbagchi.dipto@gmail.com>
2026-01-13 21:06:24 +0600

    new route made for single product with loading

---
+++ app/products/[id]/loading.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ProductDetailsSkeleton = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-6 grid grid-col-1 md:grid-cols-2 gap-10">
+      <div className="skeleton h-105 w-full rounded-xl">
+        <div className="space-y-4">
+          <div className="skeleton h-8 w-3/4"></div>
+          <div className="skeleton h-4 w-1/2"></div>
+          <div className="skeleton h-7 w-1/3"></div>
+          <div className="skeleton h-12 w-48"></div>
+          <div className="space-y-2 mt-6">
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-5/6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetailsSkeleton;
---
+++ app/products/[id]/page.jsx
@@ -0,0 +1,63 @@
+import { getSingleProduct } from "@/app/actions/server/product";
+import Image from "next/image";
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa6";
+
+const ProductDetails = async ({ params }) => {
+  const resolvedParams = await params;
+  const { id } = resolvedParams;
+  const product = (await getSingleProduct(id)) || {};
+
+  //prettier-ignore
+  const {title, image, price, discount, sold, ratings, reviews, description, info, qna}= product;
+  const discountedPrice = price - (price * discount) / 100;
+  return (
+    <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-10">
+      <div className="rounded-xl overflow-hidden">
+        <Image
+          src={image}
+          alt={title}
+          width={600}
+          height={420}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="">
+        <h1 className="text-3xl font-bold mb-3">{title}</h1>
+        <div className="flex items-center gap-3 mb-4">
+          <div className="flex text-yellow-400">
+            {Array.from({ length: 5 }, (_, i) => (
+              <FaStar
+                key={i}
+                className={i < Math.floor(ratings) ? "" : "opacity-30"}
+              />
+            ))}
+          </div>
+          <span className="text-sm text-gray-500">
+            {ratings}({reviews} reviews | {sold} sold)
+          </span>
+        </div>
+        <div className="mb-4">
+          <span className="text-2xl font-bold mr-2">${discountedPrice}</span>
+          {discount > 0 && (
+            <span className="text-sm text-gray-400 line-through ml-3">
+              ${price}
+            </span>
+          )}
+        </div>
+        <button className="btn btn-primary btn-wide flex gap-2">
+          <FaCartPlus />
+          Add to cart
+        </button>
+        <div className="mt-8 space-y-4 text-gray-400 leading-relaxed">
+          {description?.split("\n\n").map((p, i) => (
+            <p key={i}>{p}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
